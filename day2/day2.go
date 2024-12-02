65662e05051664dd1dfa03140cab6f86fdd01b0d
sara <saraacarsoy@sabanciuniv.edu>
2024-12-02 21:23:07 +0300

    day 2 p1 refactor and day 2 p2

--- day2/day2.go
+++ day2/day2.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	utils "AoC2024/inputs"
-	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -27,8 +26,20 @@ func main() {
 
 		if checkOrder(intParts) {
 			count += 1
+		} else { // part 2
+			for i := 0; i < len(intParts); i++ {
+				newArr := make([]int, len(intParts))
+				copy(newArr, intParts)
+
+				newArr = append(newArr[:i], newArr[i+1:]...)
+				if checkOrder(newArr) {
+					count += 1
+					break
+				}
+			}
 		}
 	}
+
 	print(count)
 }
 
@@ -36,40 +47,26 @@ func checkOrder(baseArray []int) bool {
 	return isSortedAscending(baseArray) || isSortedDescending(baseArray)
 }
 
-func isSortedAscending(slice []int) bool {
-	var controlArr []bool
-
+func isSortedDescending(slice []int) bool {
 	for i := 0; i < len(slice)-1; i++ {
 		if slice[i] < slice[i+1] {
 			return false
 		}
 
-		var correctDecrease bool = slice[i]-slice[i+1] == 3 || slice[i]-slice[i+1] == 2 || slice[i]-slice[i+1] == 1
-		controlArr = append(controlArr, correctDecrease)
+		if !(slice[i]-slice[i+1] == 3 || slice[i]-slice[i+1] == 2 || slice[i]-slice[i+1] == 1) {
+			return false
+		}
 	}
-	return allTrue(controlArr)
+	return true
 }
 
-func isSortedDescending(slice []int) bool {
-	var controlArr []bool
-	fmt.Println(slice)
-
+func isSortedAscending(slice []int) bool {
 	for i := 0; i < len(slice)-1; i++ {
 		if slice[i] > slice[i+1] {
 			return false
 		}
 
-		var correctIncrease bool = slice[i]-slice[i+1] == -3 || slice[i]-slice[i+1] == -2 || slice[i]-slice[i+1] == -1
-		controlArr = append(controlArr, correctIncrease)
-	}
-
-	fmt.Println(controlArr)
-	return allTrue(controlArr)
-}
-
-func allTrue(slice []bool) bool {
-	for _, val := range slice {
-		if !val {
+		if !(slice[i]-slice[i+1] == -3 || slice[i]-slice[i+1] == -2 || slice[i]-slice[i+1] == -1) {
 			return false
 		}
 	}
