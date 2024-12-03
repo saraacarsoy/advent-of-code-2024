b4fd039d6a1c013228dd47588ddc418319aed9ff
sara <saraacarsoy@sabanciuniv.edu>
2024-12-03 14:07:39 +0300

    Add day2 part2

--- day3/day3.go
+++ day3/day3.go
@@ -13,6 +13,11 @@ func main() {
 		log.Fatalf("Error reading input: %v", err)
 	}
 
+	// part1(input)
+	part2(input)
+}
+
+func part1(input []string) {
 	pattern := `mul\(([0-9]+),([0-9]+)\)`
 	re := regexp.MustCompile(pattern)
 
@@ -27,5 +32,32 @@ func main() {
 		}
 	}
 	print(count)
+}
 
+func part2(input []string) {
+	shouldMultiply := true
+	pattern := `mul\(([0-9]+),([0-9]+)\)|don't\(\)|do\(\)`
+	re := regexp.MustCompile(pattern)
+
+	count := 0
+	for _, line := range input {
+		matches := re.FindAllStringSubmatch(line, -1)
+
+		for _, match := range matches {
+			if len(match) >= 3 && match[1] != "" && match[2] != "" && shouldMultiply {
+				left, _ := strconv.Atoi(match[1])
+				right, _ := strconv.Atoi(match[2])
+
+				count += left * right
+			} else {
+				if match[0] == "don't()" {
+					shouldMultiply = false
+				} else if match[0] == "do()" {
+					shouldMultiply = true
+				}
+			}
+		}
+	}
+
+	print(count)
 }
